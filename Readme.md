43d96c51b2bc0dc22aec546ec543990888a4bded
David Corticchiato <dawi@hydra>
2013-08-22 14:40:56 +0200

    add layers, groups and drawable

---
+++ Readme.md
@@ -0,0 +1,5 @@
+
+
+![gameponent  logo](http://i1300.photobucket.com/albums/ag93/gameponents/gameponent-canvas_zpsd1e19b97.png)
+
+Component for point, vector and rect manipulation
---
+++ index.js
@@ -0,0 +1,5 @@
+exports.Canvas = require('./lib/canvas');
+exports.Layer = require('./lib/layer');
+exports.LayerGroup = require('./lib/layergroup');
+exports.ImageView = require('./lib/imageview');
+exports.Drawable = require('./lib/drawable');
